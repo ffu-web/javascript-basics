c7876582871d8eec1e56b1a7675d2615bc05b5e5
halac <halac122@gmail.com>
2022-04-14 10:43:03 +0300

    Javascript Basics

---
+++ app.js
@@ -0,0 +1,67 @@
+let age = 30;
+let userName = 'Abdi';
+let hoppies = ['Reading', 'Writing',  'Sports'];
+let job = {
+    title: 'Developer',
+    location: 'Hargeisa',
+    salary: 5000
+};
+
+// alert(userName);
+// alert(age);
+// alert(hoppies[1]);
+// alert(job.salary);
+// age = 45; 
+// let adultYear = age - 15;
+// alert(adultYear);
+
+// let adultYear; 
+
+// function calculateAdultYear() {
+//     adultYear = age - 15;
+// }
+
+// calculateAdultYear();
+// alert(adultYear);
+
+// age = 45;
+// calculateAdultYear();
+
+// alert(adultYear);
+
+// let totalAdultYears; 
+
+// function calculateAdultYears() {
+//     return age - 15;
+// }
+
+// totalAdultYears = calculateAdultYears();
+// alert(totalAdultYears);
+
+// age = 45; 
+// totalAdultYears = calculateAdultYears();
+
+// alert(totalAdultYears);
+
+let totalAdultYears;
+
+function calculateAdultYears(userAge) {
+    return userAge - 15;
+}
+
+totalAdultYears = calculateAdultYears(age);
+alert(totalAdultYears);
+
+age = 60; 
+totalAdultYears = calculateAdultYears(age);
+alert(totalAdultYears);
+
+// another example of function Paramaters 
+
+function example(parameter) {
+    alert(parameter);
+}
+
+const argument  = 'FFU';
+
+example(argument);
