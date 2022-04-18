6ac0e4c88082c07b9721b7f614206010e2927273
Yasir Ali Mohamed <yaazir143@gmail.com>
2022-04-18 19:38:43 +0300

    removed comments in the CODE!!

--- exercise.js
+++ exercise.js
@@ -21,26 +21,22 @@
 
 
 
+// proplem solution
 
+let course='Web Development'
+let price= 1500
+let mainGoals=['Web Developer','Freelancer','Leadership']
 
-
-
-
-// 1) Create three new variables:
-let course='Web Development'        //    - A variable that stores the name of an online course of your choice
-let price= 1500                     //    - A variable that stores the price of that course
-let mainGoals=['Web Developer','Freelancer','Leadership']     //    - A variable that stores the three main goals that you have, when taking this course
-
-alert(course+price+mainGoals+" \n|| reading 3 variables");  // 2) Output ("alert") the three variable values
+alert(course+price+mainGoals+" \n|| reading 3 variables");
 
 function dhamal(){
         alert(course+"\n|| from the first function \n\n"+price+"\n||  from the first function \n\n"+mainGoals+"\n|| from the first function")
         
 }
-dhamal()           // The output of my joined variables into above function
+dhamal()
 
 
-alert(mainGoals[1] + "\n|| calling array variable by index:1")  // 4) Also output the second element in your "main goals" variable
+alert(mainGoals[1] + "\n|| calling array variable by index:1")
 
 
 function custom(){
